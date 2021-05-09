faadba34c3947899cc09a01a8bdba6af0eb26625
Swapnil Kaushik <62181175+swapnilKaushik@users.noreply.github.com>
2021-05-09 22:19:05 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,16 +12,16 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
         
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="/js/app.js" defer></script>
         
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/welcome.css" rel="stylesheet">
+        <link href="css/nav.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
         <link rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.green.min.css" />
